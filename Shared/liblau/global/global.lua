bbe9bc197087a85dd87b573e46699b99107bb936
Xalalau Xubilozo <xalalau@outlook.com>
2021-06-14 22:46:47 -0300

    Remove more unnecessary code from type checks

--- Shared/liblau/global/global.lua
+++ Shared/liblau/global/global.lua
@@ -6,31 +6,19 @@ SERVER = Server and true
     Check variable type
 
     Arguments:
-        any var           = The variable to be checked
-        string checkType  = A type
-        table checkFields = { "Field name", ... } -- Check if the fields exist
+        any var = The variable to be checked
     
     Return:
         bool
 ]]
-local function is_checktype(var, checkType)
-    if not var or not checkType or type(checkType) ~= "string" then return false end
-    return type(var) == checkType or false
-end
-
-local function is_checkmeta(var, checkMeta)
-    if not var or not checkMeta or not istable(var) then return false end
-    return getmetatable(var) == checkMeta or false
-end
-
-function isbool(var) return is_checktype(var, "boolean") end
-function isfunction(var) return is_checktype(var, "function") end
-function isnumber(var) return is_checktype(var, "number") end
-function isrotator(var) return is_checkmeta(var, Rotator) end
-function isstring(var) return is_checktype(var, "string") end
-function istable(var) return is_checktype(var, "table") end
-function isvector(var) return is_checkmeta(var, Vector) end
-function isvector2d(var) return is_checkmeta(var, Vector2D) end
+function isbool(var) return type(var) == "boolean" end
+function isfunction(var) return type(var) == "function" end
+function isnumber(var) return type(var) == "number" end
+function isrotator(var) return getmetatable(var) == Rotator end
+function isstring(var) return type(var) == "string" end
+function istable(var) return type(var) == "table" end
+function isvector(var) return getmetatable(var) == Vector end
+function isvector2d(var) return getmetatable(var) == Vector2D end
 
 --[[
     Make print accept multiple arguments (detour)
