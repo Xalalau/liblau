120d5eb88ae5bc600fd6d677da5b94c8d51b0bd2
Xalalau Xubilozo <xalalau@outlook.com>
2021-06-19 15:17:47 -0300

    Add IsBasicTable()

--- Shared/liblau/global/global.lua
+++ Shared/liblau/global/global.lua
@@ -16,6 +16,7 @@ function IsFunction(var) return type(var) == "function" end
 function IsNumber(var) return type(var) == "number" end
 function IsRotator(var) return getmetatable(var) == Rotator end
 function IsString(var) return type(var) == "string" end
-function IsTable(var) return type(var) == "table" end
+function IsTable(var) return type(var) == "table" end -- All kinds of tables, even Vector() and Rotator()
+function IsBasicTable(var) return type(var) == "table" and not getmetatable(var) end -- Pure Lua table type
 function IsVector(var) return getmetatable(var) == Vector end
 function IsVector2D(var) return getmetatable(var) == Vector2D end
